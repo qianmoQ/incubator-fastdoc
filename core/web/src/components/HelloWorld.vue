dd6da35571b1ba88f5ca60e067f9e38118f980a3
qianmoQ <shicheng@ttxit.com>
2023-05-24 15:21:12 +0800

    添加 opentiny UI 框架

--- core/web/src/components/HelloWorld.vue
+++ core/web/src/components/HelloWorld.vue
@@ -1,38 +1,34 @@
-<script setup lang="ts">
-import {ref} from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
+  <div class="hello">
     <h1>{{ msg }}</h1>
-
-    <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test HMR
-        </p>
-    </div>
-
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-        >create-vue</a
-        >, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-        in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-    color: #888;
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
--- core/web/src/main.ts
+++ core/web/src/main.ts
@@ -1,5 +1,7 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
-app.mount("#app");
+app.use(router)
+app.mount('#app')
