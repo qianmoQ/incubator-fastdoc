c145e27c12eb18834c64865641fceebb90c44d20
qianmoQ <shicheng@ttxit.com>
2023-06-11 16:41:12 +0800

    添加 element-plus 框架

---
+++ core/web/src/layouts/basic/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <FastDocLayoutHeader>
+      </FastDocLayoutHeader>
+      <FastDocLayoutContent>
+      </FastDocLayoutContent>
+      <FastDocLayoutFooter>
+      </FastDocLayoutFooter>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts">
+import FastDocLayoutHeader from '@/layouts/basic/components/LayoutHeader.vue'
+import FastDocLayoutContent from '@/layouts/basic/components/LayoutContent.vue'
+import FastDocLayoutFooter from '@/layouts/basic/components/LayoutFooter.vue'
+import { ElContainer } from 'element-plus'
+
+export default {
+  name: 'FastDocLayoutContainer',
+  components: {
+    ElContainer,
+    FastDocLayoutHeader,
+    FastDocLayoutContent,
+    FastDocLayoutFooter
+  }
+}
+</script>
---
+++ core/web/src/layouts/basic/components/LayoutContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-main>
+      Content
+    </el-main>
+  </div>
+</template>
+
+<script lang="ts">
+import { ElMain } from 'element-plus'
+
+export default {
+  name: 'FastDocLayoutContent',
+  components: {
+    ElMain
+  }
+}
+</script>
